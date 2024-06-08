cb7c6fbd9a16918a22d48b928899a1eb5c64eedd
Bar .A <bar7569@gmail.com>
2024-06-08 15:56:08 +0300

    animation buff

--- src/pages/projectsAndSkills/ProjectsAndSkills.tsx
+++ src/pages/projectsAndSkills/ProjectsAndSkills.tsx
@@ -1,17 +1,21 @@
 import style from "./projectAndSkills.module.scss"
-import Projects from "./Projects";
-import Skills from "./Skills";
+import Projects from "./projects/Projects";
+import Skills from "./skills/Skills";
 
 function ProjectsAndSkills() {
     return (
         <>
             <div className={style.porjectAndSkillsContainer}>
-                <div className={style.porjectAndSkillsTitle}>
-                    <h1>Projects & Skills</h1>
-                </div>
-                <Projects />
-                <div className={style.progressContainer}>
-                    <Skills />
+                <div className={style.porjectAndSkillsWrapper}>
+                    <div className={style.porjectAndSkillsTitle}>
+                        <h1>Projects & Skills</h1>
+                    </div>
+                    <div className={style.projectsContainer}>
+                        <Projects />
+                    </div>
+                    <div className={style.progressContainer}>
+                        <Skills />
+                    </div>
                 </div>
             </div>
         </>
