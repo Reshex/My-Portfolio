36083caa9fcc7ceaea6642d245c31c8c7576d7ef
Bar .A <bar7569@gmail.com>
2024-06-12 15:34:10 +0300

    media and modularization

--- src/pages/projectsAndSkills/projects/Projects.tsx
+++ src/pages/projectsAndSkills/projects/Projects.tsx
@@ -1,21 +1,22 @@
-import projectsStyles from "./projects.module.scss"
+import projectsStyles from "./projects.module.scss";
 
 function Projects() {
-
-    function handleClick(route: string) {
-        const link = document.createElement("a") as HTMLAnchorElement;
-        link.href = route;
-        link.click();
-    }
-
     return (
         <div className={projectsStyles.projects}>
-            <button onClick={() => handleClick("https://github.com/Reshex/Weather-App")} className={projectsStyles.project1}>Weather</button>
-            <button onClick={() => handleClick("https://github.com/Reshex/DogChat")} className={projectsStyles.project2}>Dogchat</button>
-            <button onClick={() => handleClick("https://github.com/Reshex/Recipes-App")} className={projectsStyles.project3}>Recipes</button>
-            <button onClick={() => handleClick("https://github.com/Reshex/Workout-Tracker")} className={projectsStyles.project4}>Workout Tracker</button>
+            <a href="https://github.com/Reshex/Weather-App" className={projectsStyles.project1}>
+                Weather
+            </a>
+            <a href="https://github.com/Reshex/DogChat" className={projectsStyles.project2}>
+                Dogchat
+            </a>
+            <a href="https://github.com/Reshex/Recipes-App" className={projectsStyles.project3}>
+                Recipes
+            </a>
+            <a href="https://github.com/Reshex/Workout-Tracker" className={projectsStyles.project4}>
+                Workout Tracker
+            </a>
         </div>
-    )
+    );
 }
 
-export default Projects
+export default Projects;
