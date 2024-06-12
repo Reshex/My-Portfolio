1aca8dd58847a168a266e85b61abe8689a0161c7
Bar .A <bar7569@gmail.com>
2024-06-12 22:45:42 +0300

    scss

--- src/pages/projectsAndSkills/projects/Projects.tsx
+++ src/pages/projectsAndSkills/projects/Projects.tsx
@@ -3,16 +3,16 @@ import projectsStyles from "./projects.module.scss";
 function Projects() {
     return (
         <div className={projectsStyles.projects}>
-            <a href="https://github.com/Reshex/Weather-App" className={projectsStyles.project1}>
+            <a href="https://github.com/Reshex/Weather-App" className={projectsStyles.project3}>
                 Weather
             </a>
-            <a href="https://github.com/Reshex/DogChat" className={projectsStyles.project2}>
+            <a href="https://github.com/Reshex/DogChat" className={projectsStyles.project4}>
                 Dogchat
             </a>
-            <a href="https://github.com/Reshex/Recipes-App" className={projectsStyles.project3}>
+            <a href="https://github.com/Reshex/Recipes-App" className={projectsStyles.project2}>
                 Recipes
             </a>
-            <a href="https://github.com/Reshex/Workout-Tracker" className={projectsStyles.project4}>
+            <a href="https://github.com/Reshex/Workout-Tracker" className={projectsStyles.project1}>
                 Workout Tracker
             </a>
         </div>
