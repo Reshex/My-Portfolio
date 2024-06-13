1fcfe8267a16d4fbb9b52792da0d37f741dcff10
Bar .A <bar7569@gmail.com>
2024-06-13 23:21:54 +0300

    so many media query

--- src/pages/contact/Contact.tsx
+++ src/pages/contact/Contact.tsx
@@ -7,12 +7,14 @@ function Contact() {
             <div className={style.contactWrapper}>
                 <div className={style.contactTitle}>
                     <h1>Contact Me</h1>
-                    <p>Feel free to reach out to me via email or connect with me on LinkedIn or GitHub:</p>
                 </div>
-                <div className={style.contactIcons}>
-                    <a href="mailto:bar7569@gmail.com"><AiFillMail /></a>
-                    <a href={"https://www.linkedin.com/in/bar-abulher-20b22626b/"}><AiFillLinkedin /></a>
-                    <a href={"https://github.com/Reshex"}><AiFillGithub /></a>
+                <div className={style.bottomContent}>
+                    <p>Feel free to reach out to me via email or connect with me on LinkedIn or GitHub:</p>
+                    <div className={style.contactIcons}>
+                        <a href="mailto:bar7569@gmail.com"><AiFillMail /></a>
+                        <a href={"https://www.linkedin.com/in/bar-abulher-20b22626b/"}><AiFillLinkedin /></a>
+                        <a href={"https://github.com/Reshex"}><AiFillGithub /></a>
+                    </div>
                 </div>
             </div>
         </div>
