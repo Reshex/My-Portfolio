ab82af20f505d2f91f4ef627393cde4d8a516838
Bar .A <111001969+Reshex@users.noreply.github.com>
2024-12-30 01:14:30 +0200

    replace old project

--- src/pages/projectsAndSkills/projects/Projects.tsx
+++ src/pages/projectsAndSkills/projects/Projects.tsx
@@ -6,14 +6,14 @@ function Projects() {
             <a href="https://github.com/Reshex/Weather-App" className={projectsStyles.project3}>
                 Weather
             </a>
-            <a href="https://github.com/Reshex/DogChat" className={projectsStyles.project4}>
+            <a href="https://github.com/Reshex/DogChat" className={projectsStyles.project1}>
                 Dogchat
             </a>
             <a href="https://github.com/Reshex/Recipes-App" className={projectsStyles.project2}>
                 Recipes
             </a>
-            <a href="https://github.com/Reshex/Workout-Tracker" className={projectsStyles.project1}>
-                Workout Tracker
+            <a href="https://github.com/Reshex/https://github.com/Reshex/Mind-Map" className={projectsStyles.project4}>
+                Mind Map (Best)
             </a>
         </div>
     );
